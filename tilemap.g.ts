9ec78866cd0d75e69ac021924798375217b3a50e
fhsh-chin <122066507+fhsh-chin@users.noreply.github.com>
2023-01-13 21:07:52 +0800

    更新 main.blocks, tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -3,16 +3,6 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
-    export const tile2 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile3 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile4 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile5 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile6 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
     export const tile7 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile8 = image.ofBuffer(hex``);
@@ -24,6 +14,16 @@ namespace myTiles {
     export const tile11 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
@@ -53,16 +53,6 @@ namespace myTiles {
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "transparency16":return transparency16;
-            case "我的貼圖0":
-            case "tile2":return tile2;
-            case "我的貼圖1":
-            case "tile3":return tile3;
-            case "我的貼圖2":
-            case "tile4":return tile4;
-            case "我的貼圖3":
-            case "tile5":return tile5;
-            case "我的貼圖4":
-            case "tile6":return tile6;
             case "我的貼圖5":
             case "tile7":return tile7;
             case "我的貼圖6":
@@ -75,6 +65,16 @@ namespace myTiles {
             case "tile11":return tile11;
             case "我的貼圖8":
             case "tile10":return tile10;
+            case "我的貼圖0":
+            case "tile2":return tile2;
+            case "我的貼圖1":
+            case "tile3":return tile3;
+            case "我的貼圖2":
+            case "tile4":return tile4;
+            case "我的貼圖3":
+            case "tile5":return tile5;
+            case "我的貼圖4":
+            case "tile6":return tile6;
         }
         return null;
     })
